b04e0bdf7c3d5e34125b4a87a651bcef011eb3a1
user <y.dubovitsky@gmail.com>
2022-02-28 13:01:50 +0300

    added min height for category-list-item

--- src/components/main/category/category-list-item/category-list-item.component.jsx
+++ src/components/main/category/category-list-item/category-list-item.component.jsx
@@ -1,12 +1,21 @@
 import styles from './category-list-item.module.css';
 import { Link } from 'react-router-dom';
 
-const CategoryListItem = ({ imageByte, name, price }) => {
+const CategoryListItem = ({ imageByte, name }) => {
+
+  const randomInteger = (min, max) => {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+
   return (
     <Link
       to={`/shop/${name}`}
       className={styles.container}
-      style={{ backgroundImage: `url(data:image/png;base64,${imageByte})`}}>
+      style={{
+        backgroundImage: `url(data:image/png;base64,${imageByte})`,
+        minHeight: randomInteger(200, 350) + 'px'
+      }}>
       <div className={styles.name}>{name}</div>
     </Link>
   )
