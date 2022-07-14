4ec18259364bb71c0704816ec376256de306589d
user <y.dubovitsky@gmail.com>
2022-07-14 10:36:51 +0300

    Popup middleware added

---
+++ src/redux/middleware/popup.middleware.js
@@ -0,0 +1,47 @@
+import { showPopup } from "../features/popup/popup.slice";
+
+const CHECKOUT_MAKEORDER_FULFILLED_PROPERTIES = {
+  isShow: true,
+  message: 'Congratulations, your order has been placed!',
+  style: {
+    "background-image": "linear-gradient(to left top, #F8B510 50%, #28A745)"
+  }
+}
+
+const SUBSCRIBE_SUBSCRIBE_PROPERTIES = {
+  isShow: true,
+  message: 'Sorry, this feature is temporarily unavailable',
+  style: {
+    "background-image": "linear-gradient(to left top, #F8B510 50%, #4F8A8B)"
+  }
+}
+
+const CHECKOUT_DELETEORDERBYID_FULFILLED_PROPERTIES = {
+  isShow: true,
+  message: 'The order has been successfully deleted',
+  style: {
+    "background-image": "linear-gradient(to left top, #F8B510 50%, #17A2B8)"
+  }
+}
+
+const PopupMiddleware = ({dispatch, getState }) => next => action => {
+  const { type } = action;
+  switch(type) {
+    case 'checkout/makeOrder/fulfilled' : {
+      dispatch(showPopup(CHECKOUT_MAKEORDER_FULFILLED_PROPERTIES));
+      break;
+    }
+    case 'subscribe/subscribe' : {
+      dispatch(showPopup(SUBSCRIBE_SUBSCRIBE_PROPERTIES));
+      break;
+    }
+    case 'checkout/deleteOrderById/fulfilled' : {
+      dispatch(showPopup(CHECKOUT_DELETEORDERBYID_FULFILLED_PROPERTIES));
+      break;
+    }
+    default: break;
+  }
+  next(action);
+}
+
+export default PopupMiddleware;
